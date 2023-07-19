96417384678b632593e005a9f08284b7712a64e6
paola35 <135664758+prjwthpao@users.noreply.github.com>
2023-07-19 23:53:10 +0200

    Inserimento degli amici nel gioco
    Ho aggiunto gli amici e ho anche modificato il codice di goal aggiungendo la condizione dei 3 amici ottenuti.

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int amiciCount = 0;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("amici"))
+        {
+            Destroy(collision.gameObject);
+            amiciCount++;
+            Debug.Log("amiciCount: " + amiciCount);
+        }
+    }
+
+    public int GetCollectedFriends()
+    {
+        return amiciCount;
+    }
+}
